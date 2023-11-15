c3cb3635f2f900a6dd968ca4a5e95af3558cdc4d
JMedina9741 <147419480+JMedina9741@users.noreply.github.com>
2023-11-15 16:17:00 +0100

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RouterModule, UrlSegment } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        matcher: (url) => {
+          if (url.length === 1 && url[0].path.match(/^@[\w]+$/gm)) {
+            return {
+              consumed: url,
+              posParams: {
+                username: new UrlSegment(url[0].path.slice(1), {})
+              }
+            };
+          }
+      
+          return null;
+        },
+        component: ProfileComponent
+      }])],
   providers: [],
   bootstrap: [AppComponent]
 })
